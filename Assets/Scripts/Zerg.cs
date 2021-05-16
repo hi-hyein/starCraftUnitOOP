aad3ef6c60000cf6b4082d0ca431400a74e426dd
Hyein <junghi0327@gmail.com>
2021-05-17 08:55:49 +0900

    #9 커맨더 유닛 선택 테스트 코드

---
+++ Assets/Scripts/Zerg.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zerg : Commander
+{
+    private static List<Unit> myUnits;
+    public static List<Unit> MyUnits
+
+    {
+        get
+        {
+            return myUnits;
+        }
+
+        set {
+            myUnits = value;
+        }
+    }
+
+    public Zerg()
+	{
+		MyUnits = new List<Unit>();
+	}
+}
